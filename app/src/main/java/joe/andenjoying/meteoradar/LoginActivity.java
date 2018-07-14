2e27d75ac3e2ff17e279394e6e25eda6266347c7
Joseph Elder <jelde010@odu.edu>
2018-07-14 01:26:13 -0400

    Added initial gradient effect to login screen.

--- app/src/main/java/joe/andenjoying/meteoradar/LoginActivity.java
+++ app/src/main/java/joe/andenjoying/meteoradar/LoginActivity.java
@@ -1,9 +1,11 @@
 package joe.andenjoying.meteoradar;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 public class LoginActivity extends Activity {
 
@@ -11,6 +13,14 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+
+
 
         findViewById(R.id.loginButton).setOnClickListener( new View.OnClickListener()
         {
